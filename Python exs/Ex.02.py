da47c37bf0cdd25eb5c3eeff585ae5de247bb325
Cláudio Brito <cabf.brito@gmail.com>
2022-10-19 17:58:14 -0300

    correction

--- Python exs/Ex.02.py
+++
@@ -1,8 +0,0 @@
-#Date of birth
-
-year = int(input("Ano de nascimento: \n"))
-month = int(input("Mês de nascimento: \n"))
-day = int(input("Dia de nascimento: \n"))
-
-print("A pessoa nasceu em: {}/{}/{}".format(day, month, year))
-print("A pessoa nasceu no dia {} do mês {} do ano de {}.".format(day, month, year))
