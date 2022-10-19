d85432c314a4c0c1b3c703f42bdd201146d66175
Cláudio Brito <cabf.brito@gmail.com>
2022-10-19 18:00:43 -0300

    int number (predecessor and successor)

---
+++ Python exs/Ex.05.py
@@ -0,0 +1,6 @@
+#int (predecessor and successor)
+
+
+n = int(input("Digite um número: \n"))
+print("O antecessor do número {} é: {}.".format(n, n - 1))
+print("O sucessor do número {} é: {}.".format(n, n + 1))
