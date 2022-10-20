fbcecb72c8b0f9ca167679d288ab46be281269f3
Cláudio Brito <cabf.brito@gmail.com>
2022-10-20 10:12:13 -0300

    Multiples table

---
+++ Python exs/Ex.07.py
@@ -0,0 +1,5 @@
+#Student grades
+
+nota1 = float(input("Nota 1: \n"))
+nota2 = float(input("Nota 2: \n"))
+print("A média das notas {:.1f} e {:.1f} é {:.1f}!".format(nota1, nota2, ((nota1 + nota2) / 2)))
