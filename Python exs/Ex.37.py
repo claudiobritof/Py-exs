7cfb5d83b8c50c2effb09bb0ab00e7b63e7fa87d
Cláudio Brito <cabf.brito@gmail.com>
2022-10-27 10:36:35 -0300

    Number conversion

---
+++ Python exs/Ex.37.py
@@ -0,0 +1,20 @@
+#Number conversion
+
+num = int(input("Digite um número a ser convertido: \n"))
+base = int(input('''Escolha a base da conversão:
+Para binário digite: 1
+Para octal digite: 2
+Para hexadecimal digite: 3
+Sua escolha: \n'''))
+
+if base == 1:
+    print("Você escolheu binário.")
+    print("{} convertido para binário é: {}.".format(num, bin(num)[2:]))
+elif base == 2:
+    print("Você escolheu octal.")
+    print("{} convertido para octal é: {}.".format(num, oct(num)[2:]))
+elif base == 3:
+    print("Você escolheu hexadecimal.")
+    print("{} convertido para hexadecimal é: {}.".format(num, hex(num)[2:]))
+else:
+    print("Escolha uma das 3 opções.")
