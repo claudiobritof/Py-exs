e7338fd6d6449cbc5d05b1f18169e593f362beb4
Cláudio Brito <cabf.brito@gmail.com>
2022-10-28 15:04:59 -0300

    Prime Int number

---
+++ Python exs/Ex.52.py
@@ -0,0 +1,15 @@
+#Prime Int number
+
+num = int(input("Descubra se um número é primo: \n"))
+
+isPrime = True
+current = (num // 2)
+
+for i in range(current, 0, -1):
+    if num % i == 0 and i != 1:
+        isPrime = False
+
+if isPrime:
+    print("O número {} é primo.".format(num))
+else:
+    print("O número {} não é primo.".format(num))
