7d7022de173fa3741fca1fe8bca7ba4bbc7cfa65
Cláudio Brito <cabf.brito@gmail.com>
2022-10-28 15:05:12 -0300

    Majority (age)

---
+++ Python exs/Ex.54.py
@@ -0,0 +1,17 @@
+#Majority (age)
+
+from datetime import date
+''' Programa que lê o ano de nascimento de sete pessoas
+No final, mostrando quantas pessoas ainda não atingiram a maioridade
+e quantas já são maiores'''
+atual = date.today().year
+counter_young = 0
+counter_adult = 0
+for i in range(7):
+    num = int(input("Digite anos de nascimento: \n"))
+    if atual - num >= 21:
+        counter_adult += 1
+    elif atual - num < 21:
+        counter_young += 1
+print("Ao todo tivemos {} maiores e {} menores de idade.".format(
+    counter_adult, counter_young))
