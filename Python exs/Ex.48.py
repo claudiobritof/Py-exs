6a52752b36a3db6c8045a97e7fe265bfaa70d944
Cláudio Brito <cabf.brito@gmail.com>
2022-10-28 15:04:31 -0300

    Odd numbers, multiple of 3, in (1-500)

---
+++ Python exs/Ex.48.py
@@ -0,0 +1,8 @@
+#Odd numbers, multiple of 3, in (1-500)
+
+s = 0  # Contador de soma
+for num in range(1, 501):  # de 1 até 500
+    if num % 2 != 0:  # Ímpares
+        if num % 3 == 0:  # Ímpares múltiplos de 3
+            s += num  # soma = soma + num
+print("A soma dos múltiplos é: {}.".format(s))
