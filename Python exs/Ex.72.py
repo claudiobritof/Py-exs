12f63d281a1c2632544d078f5a891871d4f6f34d
Cláudio Brito <cabf.brito@gmail.com>
2022-10-29 12:37:56 -0300

    Tuples intro

---
+++ Python exs/Ex.72.py
@@ -0,0 +1,14 @@
+#Tuples intro
+
+extensos = (
+    "zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito",
+    "nove", "dez", "onze", "doze", "treze", "catorze", "quinze",
+    "dezesseis", "dezessete", "dezoito", "dezenove", "vinte"
+    )
+while True:
+    n = int(input("Digite um número de 0 a 20: \n"))
+    if n >= 0 and n <= 20:
+        print(f"Você digitou: {extensos[n]}.")
+        break
+    else:
+        print("Tente novamente.", end=" ")
