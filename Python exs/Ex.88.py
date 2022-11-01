aa9e0f971d113e27693352be73fb6af366e72882
Cláudio Brito <cabf.brito@gmail.com>
2022-11-01 07:21:07 -0300

    Lottery

---
+++ Python exs/Ex.88.py
@@ -0,0 +1,27 @@
+#Lottery
+
+
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+quantidade = int(input("Quantos jogos sortear? \n"))
+total = 1
+
+while total <= quantidade:
+    counter = 0
+    while True:
+        number = randint(1, 60)
+        if number not in lista:
+            lista.append(number)
+            counter += 1
+        if counter >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+
+for i, l in enumerate(jogos):
+    print(f"jogo {i + 1}: {l}")
+    sleep(1)
