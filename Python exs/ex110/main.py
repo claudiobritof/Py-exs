c2aefc99352fb2d71ed3fea1f08ae9af1604b3e5
Cláudio Brito <cabf.brito@gmail.com>
2022-11-05 02:05:30 -0300

    Delete main.py

--- Python exs/ex110/main.py
+++
@@ -1,8 +0,0 @@
-#Setting currency (v_final)
-from ex110 import monetarydetailed
-
-currencychose = str(input('Choose a currency: ')).upper().strip()[0:3]
-p = float(input(f'Price: {currencychose} '))
-rate = float(input('Rate: '))
-
-print(summary(p))
