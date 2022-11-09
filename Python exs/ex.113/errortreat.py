b79ea45d60db82750852076e0520a48cf60b27e2
Cláudio Brito <cabf.brito@gmail.com>
2022-11-09 09:15:42 -0300

    Error treat (Validation more detailed - int and float)

---
+++ Python exs/ex.113/errortreat.py
@@ -0,0 +1,8 @@
+#Validation more detailed (int and float)
+
+from ex113.defs import readint,readfloat
+
+i = readint('Type a integer: ')
+f = readfloat('Type a float: ')
+
+print(f'You typed the integer {i} and the float {f}.')
